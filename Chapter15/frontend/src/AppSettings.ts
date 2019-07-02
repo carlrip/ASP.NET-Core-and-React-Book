af93ad381ed7a7732fbb5620e0a0a1626666320b
carl <carl.rippon@theaccessgroup.com>
2019-07-02 05:32:03 +0100

    Changed backend name

--- Chapter15/frontend/src/AppSettings.ts
+++ Chapter15/frontend/src/AppSettings.ts
@@ -1,8 +1,8 @@
 export const server =
   process.env.REACT_APP_ENV === 'production'
-    ? 'https://qanda-backend.azurewebsites.net'
+    ? 'https://your-backend.azurewebsites.net'
     : process.env.REACT_APP_ENV === 'staging'
-    ? 'https://qanda-backend-staging.azurewebsites.net'
+    ? 'https://your-backend-staging.azurewebsites.net'
     : 'http://localhost:17525';
 
 export const webAPIUrl = `${server}/api`;
